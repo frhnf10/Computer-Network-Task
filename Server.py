b3bd3d71fa9fdc2510bee3f44f6d7bf3704d79f3
Farhan Fath <farhanfath.a.student@gmail.com>
2024-05-30 15:55:56 +0700

    chengingi

---
+++ Server.py
@@ -0,0 +1,35 @@
+#import socket module
+from socket import *
+serverSocket = socket(AF_INET, SOCK_STREAM)                         # Membuat Socket
+#Prepare a server socket
+
+webserver_name = 'localhost'
+webserver_port_number = 4750                                        # Membuat Nomor Port
+serverSocket.bind((webserver_name, webserver_port_number))          # Mengaitkan Socket Dengan Nomor Port
+serverSocket.listen(3)                                              # Siap Mendengarkan ketukan pintu dari client (Batas 3)
+
+while True:
+    #Establish the connection
+    print ('Siap Menerima Ketukan....')
+
+    # pindah ke client
+    connectionSocket, client_address = serverSocket.accept()                  # Pintu Socket dibuka oleh serverSocket, masuk dengan socket baru-
+    print('Koneksi diterima dari:', client_address)                            # namanya connectionsocket kiriman dari client, dan kemudian mereka-
+                                                                    # dapat bertukar bit
+    try:
+        message = connectionSocket.recv(1024)
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+        #Send one HTTP header line into socket
+        connectionSocket.send('HTTP/1.1 200 OK \r\n\r\n'.encode())
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.close()
+    except IOError:
+        #Send response message for the file not found
+        connectionSocket.send("HTTP/1.1 200 OK  \r\n\r\n 404 Not Found".encode())
+        #print('404 error: File not found')
+    connectionSocket.close()
+serverSocket.close()
--- Socket_ClientServer.py
+++ Socket_ClientServer.py
@@ -1,18 +1,15 @@
 from socket import *
-servername = '127.0.0.1'
-serverport = 6777
 clientsocket = socket(AF_INET, SOCK_STREAM)
+
+servername = 'localhost'
+serverport = 4750
 clientsocket.connect((servername,serverport))
 
-#sentence = input('lowercase input: ')
-#clientsocket.send(sentence.encode())
+message = "Halo, "
+clientsocket.send(message.encode())
 
-filename = "/index.html"  # Example filename
-request = f"GET {filename} HTTP/1.1\r\nHost: {servername}\r\n\r\n"
-clientsocket.send(request.encode())
-
-modifiedsentence = clientsocket.recv(1024)
-print('From Server: ', modifiedsentence.decode())
+response = clientsocket.recv(1024).decode()
+print('From Server: ', response)
 clientsocket.close()
 
 # masih membuat client dan server sederhana
